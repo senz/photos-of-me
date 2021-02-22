c62114607a018bdfa230d3aec5c448707d030f97
Brodie Blackburn <brodie.blackburn@gmail.com>
2021-02-22 13:36:40 +1000

    Download photos and write EXIF data

--- photos-of-me.py
+++ photos-of-me.py
@@ -1,5 +1,4 @@
 import html
-import json
 import logging
 import queue
 import random
@@ -8,17 +7,16 @@ import threading
 import time
 import urllib.parse
 from pathlib import Path
+from typing import List
 
 
 import click
+import dateutil.parser
+import exif
 import requests
-from requests import cookies
-from requests.models import cookiejar_from_dict
 import selenium.webdriver as webdriver
 import selenium.webdriver.chrome.options as chrome_options
-from selenium.common.exceptions import NoSuchElementException, TimeoutException
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.touch_actions import TouchActions
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait as Wait
 
@@ -30,14 +28,8 @@ logging.basicConfig(
 # WebDriverWait timeout in seconds.
 WAIT_TIMEOUT = 10
 
-# Queue for processing scraped photos.
-photo_queue = queue.SimpleQueue()
-
-
-class Sentinel:
-    """Indicates no more items in queue."""
-
-    pass
+# Queue of URLs to photo pages.
+photo_page_queue = queue.Queue()
 
 
 @click.command()
@@ -45,12 +37,15 @@ class Sentinel:
 @click.argument("password", envvar="FB_PASSWORD")
 @click.argument("directory", type=click.Path(exists=True))
 @click.option(
-    "--wait",
-    default=True,
-    type=bool,
-    help="Wait a random amount of time between requests",
+    "--workers", type=int, default=1, help="Number of concurrent worker threads"
 )
-def photos_of_me(username, password, directory, wait):
+@click.option(
+    "--dont-wait",
+    default=False,
+    type=bool,
+    help="Don't wait a brief random amount of time between requests",
+)
+def photos_of_me(username, password, directory, workers: int, dont_wait: bool):
     """Download "photos of me" to DIRECTORY, using Facebook credentials
     USERNAME and PASSWORD.
 
@@ -63,170 +58,215 @@ def photos_of_me(username, password, directory, wait):
 
         python photos-of-me.py me@mydomain.com $FB_PASSWORD
     """
-    # Create photo processing thread.
-    thread = threading.Thread(target=process_photo_queue, args=(directory, wait))
+    # Create worker threads to process photo URLs.
+    photo_page_queue_workers = [
+        threading.Thread(
+            target=process_photo_page_queue,
+            args=(username, password, directory, dont_wait),
+        )
+        for _ in range(workers)
+    ]
     # Prep the browser.
     driver = chrome_driver()
-    sign_in_to_facebook(driver, username, password)
-    go_to_first_photo(driver)
-    try:
-        thread.start()
-        # Start scraping.
-        for photo in photos(driver, wait):
-            photo_queue.put(photo)
-            logging.info("Put photo in queue")
-            # Uncomment the next line to break after first photo.
-            # break
-        photo_queue.put(Sentinel)
-    except KeyboardInterrupt:
-        photo_queue.put(Sentinel)
-    thread.join()
+    # This is the URL of the first page of "photos of you".
+    first_photos_of_you_page = get_first_photos_of_you_page(driver, username, password)
+    # Start the workers.
+    for worker in photo_page_queue_workers:
+        worker.start()
+    # You can get to all "photos of you" pages by setting the offset query parameter.
+    # We start with an offset of 0.
+    offset = 0
+    photos_of_you_url = get_offset_photos_of_you_page(first_photos_of_you_page, offset)
+    photo_urls = get_photo_urls(driver, photos_of_you_url)
+    # Keep increasing offset until we reach a page with no photos.
+    while photo_urls:
+        if not dont_wait:
+            time.sleep(random.random() + 0.2)
+        for url in photo_urls:
+            photo_page_queue.put(url)
+        offset = offset + 12
+        photos_of_you_url = get_offset_photos_of_you_page(
+            first_photos_of_you_page, offset
+        )
+        photo_urls = get_photo_urls(driver, photos_of_you_url)
+    photo_page_queue.join()
 
 
-def chrome_driver():
+def chrome_driver() -> webdriver.Chrome:
     """Returns instance of Chrome webdriver."""
     options = chrome_options.Options()
     options.add_experimental_option("w3c", False)
     options.add_argument("--disable-notifications")
     options.add_argument("--disable-infobars")
     options.add_argument("--mute-audio")
-    options.add_argument("--start-maximized")
     return webdriver.Chrome(options=options)
 
 
-def sign_in_to_facebook(driver, username, password):
+def get_first_photos_of_you_page(
+    driver: webdriver.Chrome, username: str, password: str
+) -> str:
+    """Returns URL of first "photos of you" page."""
+    sign_in_to_facebook(driver, username, password)
+    go_to_photos_of_you(driver)
+    return driver.current_url
+
+
+def get_offset_photos_of_you_page(first_page_url: str, offset: int) -> str:
+    """Returns a URL to a "photos of you page", offset by `offset`."""
+    # Parse URL into its components.
+    url_components = urllib.parse.urlparse(first_page_url)
+    # Parse URL query component.
+    query = urllib.parse.parse_qs(url_components.query)
+    # You can get to any "photos of you" pages by setting the offset query parameter.
+    query["offset"] = [str(offset)]
+    # Construct a new URL with a modified offset query component.
+    return urllib.parse.urlunparse(
+        url_components._replace(query=urllib.parse.urlencode(query, doseq=True))
+    )
+
+
+def sign_in_to_facebook(driver: webdriver.Chrome, username: str, password: str) -> None:
     """Signs in to Facebook with `username` and `password`."""
-    driver.get("https://m.facebook.com/")
+    driver.get("https://mbasic.facebook.com/")
+    title = driver.title
     driver.find_element_by_css_selector("input[name='email']").send_keys(username)
     driver.find_element_by_css_selector("input[name='pass']").send_keys(password)
-    driver.find_element_by_css_selector("button[name='login']").click()
+    driver.find_element_by_css_selector("input[name='login']").click()
     # Wait until title changes.
-    title = driver.title
     Wait(driver, timeout=WAIT_TIMEOUT).until_not(EC.title_is(title))
     # Then just go here again to skip that "one tap login" bullshit.
-    driver.get("https://m.facebook.com/")
+    driver.get("https://mbasic.facebook.com/")
     logging.info("Signed in to Bookface")
 
 
-def go_to_first_photo(driver):
-    """Navigates to first photo of you."""
-    wait = Wait(driver, timeout=WAIT_TIMEOUT)
-    wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "i.profpic"))).click()
-    wait.until(
-        EC.element_to_be_clickable((By.CSS_SELECTOR, "div.scrollAreaBody > a"))
+def go_to_photos_of_you(driver: webdriver.Chrome) -> None:
+    """Navigates to first page of "photos of you"."""
+    # Click "Menu" link.
+    driver.find_element_by_css_selector("a[accesskey='5']").click()
+    # Click "Photos" link.
+    driver.find_element_by_css_selector("div.bq > div:nth-child(2) > a").click()
+    # Click "See All (XXX)" link.
+    driver.find_element_by_css_selector(
+        "div:not([title='Uploads']) > section.ct > a"
     ).click()
-    wait.until(
-        EC.element_to_be_clickable((By.CSS_SELECTOR, "div.timeline.photos > span > a"))
-    ).click()
-    logging.info("Arrived at first photo of you")
+    logging.info("Arrived at first page of photos of you")
 
 
-def get_photo(driver):
-    """Scrapes photo details from page."""
+def get_photo_urls(driver: webdriver.Chrome, url: str) -> List[str]:
+    """Return photo URLs from `url`.
+
+    `url` is a "photos of you" page.
+    """
+    driver.get(url)
+    urls = [
+        element.get_attribute("href")
+        for element in driver.find_elements_by_css_selector("td.s > div > a")
+    ]
+    logging.info("Scraped %s photo urls from %s", len(urls), driver.current_url)
+    return urls
+
+
+def get_photo_details(driver: webdriver.Chrome, url: str) -> dict:
+    """Returns photo details from photo page `url`."""
+    driver.get(url)
     return {
         "actor": driver.find_element_by_css_selector("strong.actor").text,
-        "url": driver.find_element_by_link_text("View full size").get_attribute("href"),
         "caption": driver.find_element_by_xpath(
             "//div[@id='voice_replace_id']/.."
         ).text,
         "date": driver.find_element_by_css_selector("abbr").text,
-        # We need cookies for authenticate requests ;)
+        "full_size_url": driver.find_element_by_link_text(
+            "View full size"
+        ).get_attribute("href"),
+        # We need cookies for authenticated requests ;)
         "cookies": driver.get_cookies(),
     }
 
 
-def tap_next_photo(driver):
-    """Taps next photo button."""
-    TouchActions(driver).tap(
-        driver.find_element_by_xpath(
-            "//a[@data-sigil='touchable']/span[text()='Next']/.."
-        )
-    ).perform()
-    logging.info("Tapped on next photo")
+def process_photo_page_queue(
+    username: str, password: str, directory: str, dont_wait: bool
+):
+    """Processes pages in photo page queue.
 
+    - Spawn a Selenium webdriver
+    - Sign in to Facebook (using `username` and `password`)
+    - Process pages in queue:
+        - Get photo details
+        - Write photo to `directory`
+        - Write EXIF data to photo
 
-def photos(driver, wait):
-    """Generates photo details.
-
-    Wait a random amount of time between requests if `wait` is True.
+    Wait a random amount of time between queue items if `dont_wait` is not True.
     """
-    # Loop until there is no "Next" button to click --  I assume this would
-    # mean the end of photos.
+    driver = chrome_driver()
+    sign_in_to_facebook(driver, username, password)
     while True:
-        if wait:
-            time.sleep(random.random() + 0.3)
+        if not dont_wait:
+            time.sleep(random.random() + 0.2)
+        page = photo_page_queue.get()
         try:
-            # Wait for "View full size" link.
-            Wait(driver, timeout=WAIT_TIMEOUT).until(
-                EC.element_to_be_clickable(
-                    (
-                        By.LINK_TEXT,
-                        "View full size",
+            details = get_photo_details(driver, page)
+            # Get the REAL photo URL!
+            photo_url = get_photo_url(details["full_size_url"], details["cookies"])
+            # Extract a suitable photo filename from the URL.
+            filename = Path(urllib.parse.urlparse(photo_url).path).name
+            photo_path = Path(directory) / filename
+            # If photo does not already exist, write with EXIF data.
+            if not photo_path.exists():
+                photo = requests.get(photo_url).content
+                photo_path.open(mode="wb").write(
+                    with_exif_data(
+                        photo, details["actor"], details["caption"], details["date"]
                     )
                 )
-            )
-            photo = get_photo(driver)
-            logging.debug(photo)
-            yield photo
-        except TimeoutException:
-            logging.warning('"View full size" link did not appear')
-        try:
-            tap_next_photo(driver)
+            else:
+                logging.info("Photo already exists, skipping (%s)", photo_path)
         except NoSuchElementException:
-            logging.info("Can't find a next photo to tap")
-            break
+            # This is raised if a photo page doesn't contain photo details.
+            # Ignore, and move on to next item in queue.
+            logging.error("Error scraping details from photo page %s", page)
+            pass
+        except RuntimeError:
+            # This is raised if a URL can't be parsed from a photo redirect page.
+            # Ignore, and move on to next item in queue.
+            pass
+        photo_page_queue.task_done()
+        logging.info("Processed %s", page)
 
 
-def process_photo_queue(directory, wait: bool):
-    """Write photos in queue to `directory."""
-    logging.info("Photo processing thread started")
-    while True:
-        if wait:
-            time.sleep(random.random() + 0.3)
-        photo = photo_queue.get()
-        if photo is Sentinel:
-            break
-        process(photo, directory)
-        logging.debug("Processed %s", photo)
-    logging.info("Photo processing complete")
-
-
-def get_redirect_url(url: str, cookies: list) -> str:
-    """Returns redirect URL from URL.
+def get_photo_url(full_size_url: str, cookies: list) -> str:
+    """Returns redirect URL from `full_size_url`.
 
     The "View full size" URL redirects to the *real* image.
     """
     session = requests.Session()
     for cookie in cookies:
         session.cookies.set(name=cookie["name"], value=cookie["value"])
-    text = session.get(url).text
+    text = session.get(full_size_url).text
     try:
         return html.unescape(re.search(pattern=r';url=(.+?)"', string=text).group(1))
     except AttributeError:
         logging.error("Couldn't match redirect URL in %s", text)
-        raise
+        raise RuntimeError(f"Couldn't match redirect URL in {text}")
 
 
-def process(photo: dict, directory: str):
-    """Write photo to directory, with metadata."""
-    try:
-        # Get the REAL photo URL!
-        photo_url = get_redirect_url(photo["url"], photo["cookies"])
-    except AttributeError:
-        # Not much we can do, move on.
-        return
-    # We extract a suitable photo filename from the URL.
-    filename = Path(urllib.parse.urlparse(photo_url).path).name
-    # Write photo to file.
-    photo_path = Path(directory) / filename
-    with photo_path.open(mode="wb") as file:
-        file.write(requests.get(photo_url).content)
-    logging.info("Wrote photo to %s", photo_path)
-    # Write photo information to file (JSON).
-    json_path = Path(directory) / (filename + ".json")
-    with json_path.open(mode="w") as file:
-        file.write(json.dumps(photo, indent=2))
-    logging.info("Wrote photo information to %s", json_path)
+def with_exif_data(photo: bytes, actor: str, caption: str, date: str) -> bytes:
+    """Returns photo with EXIF data."""
+    image = exif.Image(photo)
+    if actor:
+        # Replace any non-ASCII characters.
+        image["artist"] = actor.encode(encoding="ascii", errors="replace").decode()
+    if date:
+        image["datetime_original"] = dateutil.parser.parse(date).strftime(
+            exif.DATETIME_STR_FORMAT
+        )
+    if caption:
+        # Replace any non-ASCII characters. Apparently user_comment should be
+        # able to support arbitrary data. But that doesn't seem to work with
+        # the exif package.
+        image["user_comment"] = caption.encode(
+            encoding="ascii", errors="replace"
+        ).decode()
+    return image.get_file()
 
 
 if __name__ == "__main__":
